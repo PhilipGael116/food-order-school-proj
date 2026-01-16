5dee91b19ab0464b422b0b849ce1cc138bdb54e8
Philippe Gael <gaelngando@gmail.com>
2026-01-16 14:29:11 +0100

    finshied the header

--- static-pages/components/header.php
+++ static-pages/components/header.php
@@ -2,12 +2,12 @@
     <div class="logo">
         <img src="../images/logo.png" alt="logo">
     </div>
-    <nav>
+    <nav class="nav">
         <a href="#">Home</a>
         <a href="#">Menu</a>
         <a href="#">About Us</a>
     </nav>
-    <div>
+    <div class="icons">
         <i class="fa-solid fa-cart-shopping"></i>
         <i class="fa-solid fa-user"></i>
     </div>
