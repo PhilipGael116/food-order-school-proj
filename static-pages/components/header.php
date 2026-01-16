c05220c20043f307254fe3e8e78cd9b361c69b6b
Philippe Gael <gaelngando@gmail.com>
2026-01-16 14:29:00 +0100

    finished setting up the project frontedn including the

---
+++ static-pages/components/header.php
@@ -0,0 +1,14 @@
+<header class="container">
+    <div class="logo">
+        <img src="../images/logo.png" alt="logo">
+    </div>
+    <nav>
+        <a href="#">Home</a>
+        <a href="#">Menu</a>
+        <a href="#">About Us</a>
+    </nav>
+    <div>
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-solid fa-user"></i>
+    </div>
+</header>
